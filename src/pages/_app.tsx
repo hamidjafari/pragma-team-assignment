e41e637c3c86991385734a5258daddf20266c546
Hamidreza Jafari <hamidreza.jafari.abshouri@gmail.com>
2023-05-17 16:45:45 +0330

    Feat/implement next auth
    * build: add next-auth and prisma
    
    * feat: add env typing
    
    * feat: add prisma schema for auth
    
    * feat: generate prisma migration
    
    * feat: implement basic next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,26 @@ import "@styles/globals.css";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { ReactElement, ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
 	getLayout?: (page: ReactElement) => ReactNode;
 };
 
-type AppPropsWithLayout = AppProps & {
+type AppPropsWithLayout = AppProps<{ session: Session }> & {
 	Component: NextPageWithLayout;
 };
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+export default function App({
+	Component,
+	pageProps: { session, ...pageProps },
+}: AppPropsWithLayout) {
 	const getLayout = Component.getLayout ?? ((page) => page);
 
-	return getLayout(<Component {...pageProps} />);
+	return getLayout(
+		<SessionProvider session={session}>
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
+import { useSession, signIn, signOut } from "next-auth/react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+	const { data: session, status } = useSession();
 	return (
 		<main
-			className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}></main>
+			className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
+			<h1>{status}</h1>
+			{session ? (
+				<>
+					Signed in as {session?.user?.name} <br />
+					<button onClick={() => signOut()}>Sign out</button>
+				</>
+			) : (
+				<>
+					Not signed in <br />
+					<button onClick={() => signIn()}>Sign in</button>
+				</>
+			)}
+		</main>
 	);
 }
