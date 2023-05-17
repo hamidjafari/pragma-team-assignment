81236496edf0d3b9ac13b2470d334c8d0580d5d7
Hamidreza Jafari <hamidreza.jafari.abshouri@gmail.com>
2023-05-17 17:21:11 +0430

    feat: implement basic next-auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import { AuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions: AuthOptions = {
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+		GithubProvider({
+			clientId: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+		}),
+	],
+	secret: process.env.NEXTAUTH_SECRET,
+};
+
+console.log(NextAuth(authOptions));
+
+export default NextAuth(authOptions);
