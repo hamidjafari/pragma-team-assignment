cd8b302aa220732739997e96c3e2b8add697f31d
Hamidreza Jafari <hamidreza.jafari.abshouri@gmail.com>
2023-05-18 11:08:31 +0430

    feat: add default layout to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,26 @@ import { getServerSession } from "next-auth";
 import { Inter } from "next/font/google";
 import { authOptions } from "./api/auth/[...nextauth]";
 import { GetServerSidePropsContext } from "next";
+import { NextPageWithLayout } from "./_app";
+import DefaultLayout from "@layout/DefaultLayout";
+import { ReactElement } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+const Home: NextPageWithLayout = () => {
 	return (
 		<main
 			className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
 			Home
 		</main>
 	);
-}
+};
+
+Home.getLayout = function getLayout(page: ReactElement) {
+	return <DefaultLayout>{page}</DefaultLayout>;
+};
+
+export default Home;
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
 	const session = await getServerSession(context.req, context.res, authOptions);
