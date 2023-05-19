8d4b3d4f93cd130807fbd6a86795e2cace0ce095
Hamidreza Jafari <hamidreza.jafari.abshouri@gmail.com>
2023-05-19 07:15:00 +0430

    feat: add sign out to navbar

--- src/app/components/modules/Navbar.tsx
+++ src/app/components/modules/Navbar.tsx
@@ -1,12 +1,15 @@
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 export default function Navbar() {
 	const { data } = useSession();
 
 	return (
 		<nav
-			className={`flex w-full p-3 backdrop-blur shadow-2xl shadow-slate-100 fixed left-0 top-0 right-0 bg-white bg-opacity-70`}>
+			className={`flex w-full p-3 backdrop-blur shadow-2x fixed left-0 top-0 right-0  bg-opacity-70 justify-between`}>
 			<h3>Welcome {data?.user?.name}</h3>
+			<h2 className="cursor-pointer" onClick={() => signOut()}>
+				Sign Out
+			</h2>
 		</nav>
 	);
 }
